bb0bd8f5fb5c2716f3896f4649f845256fb5d8df
Hydral8 <sbae703@gmail.com>
2026-04-13 23:52:52 -0700

    added conv2d im2col

--- numpy_version/mnist_cnn.py
+++ numpy_version/mnist_cnn.py
@@ -25,8 +25,117 @@ def _im2col_col2im_indices_along_dim(input_size, kernel_size, stride, padding, d
 
     return block_indices + kernel_deltas # shape (kernel_size, n_blocks)
 
+def _check_size_2(variable, name):
+    # check if variable has length of 2 (i.e. padding, stride, etc)
+    if len(variable) != 2:
+        raise ValueError(f"{name} must have length of 2 but got {len(variable)}")
+
+def _is_positive(variable, name):
+    # check if variable is positive
+    if not all(x > 0 for x in variable):
+        raise ValueError(f"{name} must be positive but got {variable}")
+    
+def _im_2col_fast(x, kernel_size, stride, padding, dilation, pool = None):
+
+    # x is (batch_size, in_channels, in_height, in_width)
+
+    _check_size_2(kernel_size, "kernel_size")
+    _check_size_2(dilation, "dilation")
+    _check_size_2(padding, "padding")
+    _check_size_2(stride, "stride")
+
+    _is_positive(kernel_size, "kernel_size")
+    _is_positive(dilation, "dilation")
+    _is_positive(padding, "padding")
+    _is_positive(stride, "stride")
+
+    # get shape and ndims
+    shape = x.shape
+    ndims = len(x.shape)
+
+    # check that ndims is 3 or 4 (image) or batched images (other dims cannot be 0 except for batch)
+    if ndims not in [3, 4] and all(s > 0 for s in shape[-3:]):
+        raise ValueError(f"x must have 3 or 4 dimensions but got {ndims}")
+
+    # calculate output shape check
+    output_shape = tuple(
+        (shape[-2] + 2 * padding[0] - dilation[0] * (kernel_size[0] - 1) - 1) // stride[0] + 1,
+        (shape[-1] + 2 * padding[1] - dilation[1] * (kernel_size[1] - 1) - 1) // stride[1] + 1
+    )
+
+    # output_shape check
+    if not all(s > 0 for s in output_shape):
+        raise ValueError(
+            f"output_shape must be positive but got {output_shape} for input shape {shape}, "
+            f"kernel_size {kernel_size}, "
+            f"stride {stride}, "
+            f"padding {padding}, "
+            f"dilation {dilation}"
+        )
+
+    # okay input dims and output_shape okay. Batch if not already batched
+    if ndims == 3:
+        x = x[None, :, :, :]
+    
+    # get shape and ndims
+    batch_size, in_channels, n_rows, n_cols = x.shape
+    kernel_height, kernel_width = kernel_size
+
+    # indices determined for padded input so need to pad x
+    padded_x = np.pad(x, ((0, 0), (0, 0), (padding[0], padding[0]), (padding[1], padding[1])), mode="constant")
+
+    # now get indices
+    h_indices = _im2col_col2im_indices_along_dim(n_rows, kernel_height, stride[0], padding[0], dilation[0])
+    w_indices = _im2col_col2im_indices_along_dim(n_cols, kernel_width, stride[1], padding[1], dilation[1])
+
+    # now collect indices across dim
+
+    # expand h_indices such that we can broadcast with w_indices
+    h_indices = h_indices[:, :, None, None] # shape (kernel_height, n_h_blocks, 1, 1)
+
+    blocks = padded_x[:, :, h_indices, w_indices] # shape (batch_size, in_channels, kernel_height, n_h_blocks, kernel_width, n_w_blocks)
+    # before reshape must place kernel_dims next to each other so that those indices are grouped
+    blocks.transpose(0, 1, 2, 4, 3, 5) # shape (batch_size, in_channels, kernel_height, kernel_width, n_h_blocks, n_w_blocks)
+
+
+    # now get num blocks col and width
+    n_h_blocks = h_indices.shape[1]
+    n_w_blocks = w_indices.shape[1]
+
+    # now reshape based on whether we are doing a pooling operation or not
+
+    if pool is None:
+        blocks = blocks.reshape(batch_size, in_channels * kernel_size[0] * kernel_size[1], n_h_blocks * n_w_blocks)
+        blocks = blocks.transpose(0, 2, 1) # shape (batch_size, n_h_blocks * n_w_blocks, in_channels * kernel_size[0] * kernel_size[1])
+
+        # now in form of (batch, num_blocks, block). Can now matmul. 
+        # output shape needed to know how to reshape back
+        return blocks, output_shape
+    else:
+        # you pool for each kernel, transpose back to kernel mode
+        blocks = blocks.transpose(0, 1, 3, 5, 2, 4) # shape (batch_size, in_channels, n_h_blocks, n_w_blocks, kernel_height, kernel_width)
+        blocks = blocks.reshape(batch_size, in_channels, n_h_blocks, n_w_blocks, -1)
+        if pool == "max":
+            blocks = blocks.max(axis=-1) # shape (batch_size, in_channels, n_h_blocks, n_w_blocks)
+        elif pool == "mean":
+            blocks = blocks.mean(axis=-1) # shape (batch_size, in_channels, n_h_blocks, n_w_blocks)
+        else:
+            raise ValueError(f"pool must be 'max' or 'mean' but got {pool}")
+
+        # output shape not needed for pooling but can be supplied
+        output_shape = (batch_size, in_channels, n_h_blocks, n_w_blocks)
+        return blocks, output_shape
+        
+        
+            
+
+        
+        
+
+
     
 
+    
     
 
     
